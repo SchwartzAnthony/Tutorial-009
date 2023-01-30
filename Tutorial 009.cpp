5ce5d3e4fcf95d19e1b77c95edca6438b2471d86
SchwartzAJ <schwartzanthonyj@gmail.com>
2023-01-30 16:05:35 +0100

    Add project files.

---
+++ Tutorial 009.cpp
@@ -0,0 +1,16 @@
+// Tutorial 009.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+// Multiple Files
+
+int add(int, int); 
+
+int main()
+{
+    std::cout << "The sum of 3 and 4 is: " << add(3, 4) << '\n';
+    
+    return 0; 
+}
+
